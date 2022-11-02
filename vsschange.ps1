3bb342b59bafee07f51c6efff534391b5f3f57bf
Alex <misfile@gmail.com>
2022-11-02 13:04:30 -0400

    Initial set up

---
+++ vsschange.ps1
@@ -0,0 +1,28 @@
+﻿$drives = vssadmin list shadowstorage 
+
+$testText = 'Peter <peter@gmail.com>, Paul <paul@gmail.com>' 
+
+$pattern = '(?<=\().?(?=:\))'
+
+$reg = [regex]::Matches($drives, $pattern).Value
+
+$start = "("
+$end = ":)"
+
+Write-host($reg)
+
+$count = 0
+
+while($count -ne $reg.Count){
+    
+    $target = $reg[$count] +":" 
+
+    $count++
+
+    $storage = $reg[$count] +":"
+
+    $count++
+
+    vssadmin resize shadowstorage /For=$target /On=$storage /MaxSize=20%
+
+}
